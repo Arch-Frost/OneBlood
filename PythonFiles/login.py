84b514bdf266ffec93cb8da2b942804b836bab0a
Arch-Frost <105542594+Arch-Frost@users.noreply.github.com>
2023-01-02 00:29:32 +0500

    Merged GUI and all the pages.

--- PythonFiles/login.py
+++ PythonFiles/login.py
@@ -1,4 +1,4 @@
-# Form implementation generated from reading ui file 'signup_page.ui'
+# Form implementation generated from reading ui file 'login.ui'
 #
 # Created by: PyQt6 UI code generator 6.4.0
 #
@@ -10,139 +10,239 @@ from PyQt6 import QtCore, QtGui, QtWidgets
 import mysql.connector
 from dashboard import Ui_MainWindow as dashboard
 
-class Ui_MainWindow(object):
+class Ui_MainWindow(QtWidgets.QMainWindow):
     mydb = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="qwerty123",
+            password="psudo",
             database="login")
-
+        
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(475, 384)
+        MainWindow.resize(955, 589)
+        MainWindow.setMinimumSize(QtCore.QSize(955, 589))
+        MainWindow.setMaximumSize(QtCore.QSize(955, 589))
+        MainWindow.setStyleSheet("background-color: rgb(255, 80, 97);\n"
+"background-color: rgb(232, 89, 78);\n"
+"background-color: rgb(247, 247, 247);\n"
+"background-color: rgb(255, 114, 128);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.stackedWidget = QtWidgets.QStackedWidget(self.centralwidget)
-        self.stackedWidget.setGeometry(QtCore.QRect(10, 10, 461, 371))
+        self.stackedWidget.setGeometry(QtCore.QRect(390, 120, 461, 371))
+        self.stackedWidget.setStyleSheet("background-color: rgb(255, 255, 255);")
         self.stackedWidget.setObjectName("stackedWidget")
         self.page_1 = QtWidgets.QWidget()
         self.page_1.setObjectName("page_1")
         self.usernamelabel = QtWidgets.QLabel(self.page_1)
-        self.usernamelabel.setGeometry(QtCore.QRect(130, 50, 55, 16))
+        self.usernamelabel.setGeometry(QtCore.QRect(100, 140, 81, 16))
         self.usernamelabel.setObjectName("usernamelabel")
         self.passwordlabel = QtWidgets.QLabel(self.page_1)
-        self.passwordlabel.setGeometry(QtCore.QRect(130, 100, 55, 16))
+        self.passwordlabel.setGeometry(QtCore.QRect(100, 200, 91, 16))
         self.passwordlabel.setObjectName("passwordlabel")
         self.loginButton = QtWidgets.QPushButton(self.page_1)
-        self.loginButton.setGeometry(QtCore.QRect(180, 160, 93, 28))
+        self.loginButton.setGeometry(QtCore.QRect(190, 240, 93, 28))
+        self.loginButton.setStyleSheet("background-color: rgb(255, 114, 128);\n"
+"gridline-color: rgb(255, 0, 0);\n"
+"color: rgb(0, 0, 127);\n"
+"font: 700 9pt \"Segoe UI\";\n"
+"border-radius: 4px 4px 4px 4px;")
         self.loginButton.setObjectName("loginButton")
-        self.loginButton.clicked.connect(self.login)
-        self.signupbutton = QtWidgets.QCommandLinkButton(self.page_1)
-        self.signupbutton.setGeometry(QtCore.QRect(130, 220, 222, 48))
+        self.signupbutton = QtWidgets.QPushButton(self.page_1)
+        self.signupbutton.setGeometry(QtCore.QRect(190, 310, 91, 31))
+        self.signupbutton.setStyleSheet("background-color: rgb(255, 114, 128);\n"
+"font: 700 9pt \"Segoe UI\";\n"
+"color: rgb(0, 0, 127);\n"
+"border-radius: 4px 4px 4px 4px;")
         self.signupbutton.setObjectName("signupbutton")
-        self.signupbutton.clicked.connect(self.switch_page)
         self.usernameTextField = QtWidgets.QLineEdit(self.page_1)
-        self.usernameTextField.setGeometry(QtCore.QRect(230, 50, 113, 22))
+        self.usernameTextField.setGeometry(QtCore.QRect(210, 140, 181, 21))
+        self.usernameTextField.setStyleSheet("color: rgb(0, 0, 127);\n"
+"selection-color: rgb(0, 0, 127);\n"
+"border-color: rgb(0, 0, 127);")
         self.usernameTextField.setMaxLength(15)
         self.usernameTextField.setObjectName("usernameTextField")
         self.passwordtextfield = QtWidgets.QLineEdit(self.page_1)
-        self.passwordtextfield.setGeometry(QtCore.QRect(230, 100, 113, 22))
+        self.passwordtextfield.setGeometry(QtCore.QRect(210, 200, 181, 22))
+        self.passwordtextfield.setStyleSheet("color: rgb(0, 0, 127);")
         self.passwordtextfield.setMaxLength(255)
         self.passwordtextfield.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
         self.passwordtextfield.setObjectName("passwordtextfield")
+        self.label_3 = QtWidgets.QLabel(self.page_1)
+        self.label_3.setGeometry(QtCore.QRect(220, 280, 63, 20))
+        self.label_3.setObjectName("label_3")
+        self.label_5 = QtWidgets.QLabel(self.page_1)
+        self.label_5.setGeometry(QtCore.QRect(130, 10, 71, 111))
+        self.label_5.setText("")
+        self.label_5.setPixmap(QtGui.QPixmap("../Images/pink profile image 80 cropped.jpg"))
+        self.label_5.setObjectName("label_5")
+        self.label_7 = QtWidgets.QLabel(self.page_1)
+        self.label_7.setGeometry(QtCore.QRect(212, 39, 171, 61))
+        self.label_7.setStyleSheet("font: 300 24pt \"Yu Gothic Light\";\n"
+"color: rgb(0, 0, 127);")
+        self.label_7.setObjectName("label_7")
         self.stackedWidget.addWidget(self.page_1)
         self.page_2 = QtWidgets.QWidget()
         self.page_2.setObjectName("page_2")
         self.username_signup_label = QtWidgets.QLabel(self.page_2)
-        self.username_signup_label.setGeometry(QtCore.QRect(100, 10, 55, 16))
+        self.username_signup_label.setGeometry(QtCore.QRect(100, 60, 71, 16))
+        self.username_signup_label.setStyleSheet("color: rgb(0, 0, 0);\n"
+"color: rgb(0, 0, 127);")
         self.username_signup_label.setObjectName("username_signup_label")
         self.username_signuptextfield = QtWidgets.QLineEdit(self.page_2)
-        self.username_signuptextfield.setGeometry(QtCore.QRect(190, 10, 113, 22))
+        self.username_signuptextfield.setGeometry(QtCore.QRect(250, 60, 113, 22))
+        self.username_signuptextfield.setStyleSheet("border-color: rgb(0, 0, 127);\n"
+"color: rgb(0, 0, 127);")
         self.username_signuptextfield.setMaxLength(15)
         self.username_signuptextfield.setCursorMoveStyle(QtCore.Qt.CursorMoveStyle.LogicalMoveStyle)
         self.username_signuptextfield.setObjectName("username_signuptextfield")
         self.website_signup_label = QtWidgets.QLabel(self.page_2)
-        self.website_signup_label.setGeometry(QtCore.QRect(100, 40, 55, 16))
+        self.website_signup_label.setGeometry(QtCore.QRect(100, 90, 61, 16))
+        self.website_signup_label.setStyleSheet("color: rgb(0, 0, 0);")
         self.website_signup_label.setObjectName("website_signup_label")
         self.website_signuptextfield = QtWidgets.QLineEdit(self.page_2)
-        self.website_signuptextfield.setGeometry(QtCore.QRect(190, 40, 113, 22))
+        self.website_signuptextfield.setGeometry(QtCore.QRect(250, 90, 113, 22))
+        self.website_signuptextfield.setStyleSheet("color: rgb(0, 0, 127);")
         self.website_signuptextfield.setMaxLength(255)
         self.website_signuptextfield.setObjectName("website_signuptextfield")
         self.email_signup_label = QtWidgets.QLabel(self.page_2)
-        self.email_signup_label.setGeometry(QtCore.QRect(110, 70, 55, 16))
+        self.email_signup_label.setGeometry(QtCore.QRect(100, 130, 55, 16))
+        self.email_signup_label.setStyleSheet("color: rgb(0, 0, 0);")
         self.email_signup_label.setObjectName("email_signup_label")
         self.email_signuptextfield = QtWidgets.QLineEdit(self.page_2)
-        self.email_signuptextfield.setGeometry(QtCore.QRect(190, 70, 113, 22))
+        self.email_signuptextfield.setGeometry(QtCore.QRect(250, 120, 113, 22))
+        self.email_signuptextfield.setStyleSheet("color: rgb(0, 0, 127);")
         self.email_signuptextfield.setMaxLength(100)
         self.email_signuptextfield.setObjectName("email_signuptextfield")
         self.city_signup_label = QtWidgets.QLabel(self.page_2)
-        self.city_signup_label.setGeometry(QtCore.QRect(110, 110, 55, 16))
+        self.city_signup_label.setGeometry(QtCore.QRect(100, 160, 55, 21))
+        self.city_signup_label.setStyleSheet("color: rgb(0, 0, 0);")
         self.city_signup_label.setObjectName("city_signup_label")
         self.phone_signup_label = QtWidgets.QLabel(self.page_2)
-        self.phone_signup_label.setGeometry(QtCore.QRect(110, 150, 55, 16))
+        self.phone_signup_label.setGeometry(QtCore.QRect(100, 200, 55, 16))
+        self.phone_signup_label.setStyleSheet("color: rgb(0, 0, 0);")
         self.phone_signup_label.setObjectName("phone_signup_label")
         self.phone_signuptextfield = QtWidgets.QLineEdit(self.page_2)
-        self.phone_signuptextfield.setGeometry(QtCore.QRect(190, 150, 113, 22))
+        self.phone_signuptextfield.setGeometry(QtCore.QRect(250, 200, 113, 22))
+        self.phone_signuptextfield.setStyleSheet("color: rgb(0, 0, 127);")
         self.phone_signuptextfield.setMaxLength(11)
         self.phone_signuptextfield.setFrame(True)
         self.phone_signuptextfield.setObjectName("phone_signuptextfield")
         self.password_signup_label = QtWidgets.QLabel(self.page_2)
-        self.password_signup_label.setGeometry(QtCore.QRect(110, 200, 55, 16))
+        self.password_signup_label.setGeometry(QtCore.QRect(100, 240, 71, 16))
+        self.password_signup_label.setStyleSheet("color: rgb(0, 0, 0);")
         self.password_signup_label.setObjectName("password_signup_label")
         self.password_signuptextfield = QtWidgets.QLineEdit(self.page_2)
-        self.password_signuptextfield.setGeometry(QtCore.QRect(190, 190, 113, 22))
+        self.password_signuptextfield.setGeometry(QtCore.QRect(250, 240, 113, 22))
+        self.password_signuptextfield.setStyleSheet("color: rgb(0, 0, 127);")
         self.password_signuptextfield.setText("")
         self.password_signuptextfield.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
         self.password_signuptextfield.setReadOnly(False)
         self.password_signuptextfield.setObjectName("password_signuptextfield")
         self.confirmpassword_signup_label = QtWidgets.QLabel(self.page_2)
-        self.confirmpassword_signup_label.setGeometry(QtCore.QRect(80, 240, 101, 16))
+        self.confirmpassword_signup_label.setGeometry(QtCore.QRect(100, 270, 141, 16))
+        self.confirmpassword_signup_label.setStyleSheet("color: rgb(0, 0, 0);")
         self.confirmpassword_signup_label.setObjectName("confirmpassword_signup_label")
         self.confirmpassword_signuptextfield = QtWidgets.QLineEdit(self.page_2)
-        self.confirmpassword_signuptextfield.setGeometry(QtCore.QRect(190, 240, 113, 22))
+        self.confirmpassword_signuptextfield.setGeometry(QtCore.QRect(250, 270, 113, 22))
+        self.confirmpassword_signuptextfield.setStyleSheet("color: rgb(0, 0, 127);")
         self.confirmpassword_signuptextfield.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
         self.confirmpassword_signuptextfield.setObjectName("confirmpassword_signuptextfield")
         self.signup_pushbutton = QtWidgets.QPushButton(self.page_2)
-        self.signup_pushbutton.setGeometry(QtCore.QRect(200, 280, 93, 28))
+        self.signup_pushbutton.setGeometry(QtCore.QRect(180, 300, 93, 28))
+        self.signup_pushbutton.setStyleSheet("background-color: rgb(255, 114, 128);\n"
+"gridline-color: rgb(255, 0, 0);\n"
+"color: rgb(0, 0, 127);\n"
+"font: 700 9pt \"Segoe UI\";\n"
+"border-radius: 4px 4px 4px 4px;")
         self.signup_pushbutton.setObjectName("signup_pushbutton")
-        self.signup_pushbutton.clicked.connect(self.signup)
         self.comboBox = QtWidgets.QComboBox(self.page_2)
-        self.comboBox.setGeometry(QtCore.QRect(190, 110, 111, 21))
+        self.comboBox.setGeometry(QtCore.QRect(250, 160, 111, 21))
+        self.comboBox.setStyleSheet("color: rgb(0, 0, 0);\n"
+"color: rgb(0, 0, 127);")
         self.comboBox.setObjectName("comboBox")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
-        self.signin_LinkButton = QtWidgets.QCommandLinkButton(self.page_2)
-        self.signin_LinkButton.setGeometry(QtCore.QRect(140, 320, 185, 41))
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.signin_LinkButton = QtWidgets.QPushButton(self.page_2)
+        self.signin_LinkButton.setGeometry(QtCore.QRect(290, 340, 71, 31))
+        self.signin_LinkButton.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"border-style: outset;\n"
+"border-color: rgb(255, 255, 255);\n"
+"color: rgb(0, 0, 127);\n"
+"font: 9pt \"Sitka Small\";\n"
+"selection-color: rgb(85, 170, 255);\n"
+"")
         self.signin_LinkButton.setIconSize(QtCore.QSize(20, 20))
+        self.signin_LinkButton.setCheckable(False)
         self.signin_LinkButton.setObjectName("signin_LinkButton")
-        self.signin_LinkButton.clicked.connect(self.switch_page)
+        self.label_4 = QtWidgets.QLabel(self.page_2)
+        self.label_4.setGeometry(QtCore.QRect(110, 340, 171, 31))
+        self.label_4.setObjectName("label_4")
+        self.label_6 = QtWidgets.QLabel(self.page_2)
+        self.label_6.setGeometry(QtCore.QRect(80, 10, 331, 41))
+        self.label_6.setStyleSheet("font: 700 italic 9pt \"Segoe UI\";")
+        self.label_6.setObjectName("label_6")
         self.stackedWidget.addWidget(self.page_2)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(100, 90, 291, 361))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("../Images/resized 300.jpg"))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(100, 380, 291, 111))
+        self.label_2.setStyleSheet("background-color: rgb(0, 0, 127);\n"
+"font: 24pt \"Yu Gothic\";\n"
+"font: 700 16pt \"Yu Gothic\";\n"
+"background-color: rgb(0, 0, 63);\n"
+"color: rgb(255, 255, 255);\n"
+"")
+        self.label_2.setObjectName("label_2")
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
         self.stackedWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        self.loginButton.clicked.connect(self.login)
+        self.signupbutton.clicked.connect(self.switch_page)
+        self.signup_pushbutton.clicked.connect(self.signup)
+        self.signin_LinkButton.clicked.connect(self.switch_page)
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.usernamelabel.setText(_translate("MainWindow", "username"))
-        self.passwordlabel.setText(_translate("MainWindow", "password"))
-        self.loginButton.setText(_translate("MainWindow", "login"))
-        self.signupbutton.setText(_translate("MainWindow", "signup"))
-        self.username_signup_label.setText(_translate("MainWindow", "username"))
-        self.website_signup_label.setText(_translate("MainWindow", "website"))
-        self.email_signup_label.setText(_translate("MainWindow", "email"))
-        self.city_signup_label.setText(_translate("MainWindow", "city"))
-        self.phone_signup_label.setText(_translate("MainWindow", "phone"))
-        self.password_signup_label.setText(_translate("MainWindow", "password"))
-        self.confirmpassword_signup_label.setText(_translate("MainWindow", "confirm password"))
+        self.usernamelabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700; color:#00007f;\">Username</span></p></body></html>"))
+        self.passwordlabel.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700; color:#00007f;\">Password</span></p></body></html>"))
+        self.loginButton.setText(_translate("MainWindow", "Login"))
+        self.signupbutton.setText(_translate("MainWindow", "Sign Up!"))
+        self.label_3.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700; color:#00007f;\">  OR</span></p></body></html>"))
+        self.label_7.setText(_translate("MainWindow", "LOGIN"))
+        self.username_signup_label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700;\">Username</span></p></body></html>"))
+        self.website_signup_label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700; color:#00007f;\">Website</span></p></body></html>"))
+        self.email_signup_label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700; color:#00007f;\">Email</span></p></body></html>"))
+        self.city_signup_label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700; color:#00007f;\">City</span></p></body></html>"))
+        self.phone_signup_label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700; color:#00007f;\">Phone</span></p></body></html>"))
+        self.password_signup_label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700; color:#00007f;\">Password</span></p></body></html>"))
+        self.confirmpassword_signup_label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-weight:700; color:#00007f;\">Confirm Password</span></p></body></html>"))
         self.signup_pushbutton.setText(_translate("MainWindow", "Sign Up"))
         self.comboBox.setItemText(0, _translate("MainWindow", "Islamabad"))
         self.comboBox.setItemText(1, _translate("MainWindow", "Lahore"))
         self.comboBox.setItemText(2, _translate("MainWindow", "Karachi"))
-        self.signin_LinkButton.setText(_translate("MainWindow", "Sign In"))
+        self.comboBox.setItemText(3, _translate("MainWindow", "Sialkot"))
+        self.comboBox.setItemText(4, _translate("MainWindow", "Faisalabad"))
+        self.comboBox.setItemText(5, _translate("MainWindow", "Multan"))
+        self.comboBox.setItemText(6, _translate("MainWindow", "Quetta"))
+        self.comboBox.setItemText(7, _translate("MainWindow", "Peshawar"))
+        self.signin_LinkButton.setText(_translate("MainWindow", " Sign In."))
+        self.label_4.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" color:#00007f;\">Already have an account!</span></p></body></html>"))
+        self.label_6.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:20pt; color:#00007f;\">USER INFORMATION</span></p></body></html>"))
+        self.label_2.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:22pt; font-weight:400; color:#ff7280;\">@ONEBLOOD</span></p></body></html>"))
 
     def switch_page(self):
         if self.stackedWidget.currentIndex() == 0:
@@ -162,9 +262,9 @@ class Ui_MainWindow(object):
         # Checking if the password is matched or not
         if(password != confirmpassword):
             print("password not matched")
-            self.errormsg_signup_label = QtWidgets.QLabel(self.centralwidget)
-            self.errormsg_signup_label.setGeometry(QtCore.QRect(250,530,55, 16))
-            self.errormsg_signup_label.setObjectName("password not matched")
+            QtWidgets.QMessageBox.warning(self, "Error", "Password Not Matched")
+            self.password_signuptextfield.setText("")
+            self.confirmpassword_signuptextfield.setText("")
     
         else:
             # Adding the data to the database
@@ -184,7 +284,6 @@ class Ui_MainWindow(object):
             print(mycursor.fetchall())
 
             self.switch_page()
-
 
     def login(self):
         login_status = False
@@ -216,7 +315,6 @@ class Ui_MainWindow(object):
         self.ui.setupUi(self.dashboard)
         self.dashboard.show()
         MainWindow.hide()
-    
 
 
 if __name__ == "__main__":
